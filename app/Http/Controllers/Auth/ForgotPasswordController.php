850f8500e5864c36bb298575856a5006171a96ca
PeterNdeke <ndekepeter2015@gmail.com>
2020-01-27 22:51:11 +0100

    commit

--- app/Http/Controllers/Auth/ForgotPasswordController.php
+++ app/Http/Controllers/Auth/ForgotPasswordController.php
@@ -10,7 +10,7 @@ use Illuminate\Http\Request;
 
 class ForgotPasswordController extends Controller
 {
-    use MailTrait;
+   // use MailTrait;
     /*
     |--------------------------------------------------------------------------
     | Password Reset Controller
@@ -33,28 +33,28 @@ class ForgotPasswordController extends Controller
     {
         $this->middleware('guest');
     }
-    public function showLinkRequestForm()
-    {
-        $data['page_title'] = "Reset Password";
-        return view('auth.passwords.email',$data);
-    }
-    public function sendResetLinkEmail(Request $request)
-    {
-        $this->validateEmail($request);
+    // public function showLinkRequestForm()
+    // {
+    //     $data['page_title'] = "Reset Password";
+    //     return view('auth.passwords.email',$data);
+    // }
+    // public function sendResetLinkEmail(Request $request)
+    // {
+    //     $this->validateEmail($request);
 
-        $us = User::whereEmail($request->email)->count();
-        if ($us == 0)
-        {
-            session()->flash('message','We can\'t find a user with that e-mail address.');
-            session()->flash('type','danger');
-            return redirect()->back();
-        }else{
-           $this->userPasswordReset($request->email);
+    //     $us = User::whereEmail($request->email)->count();
+    //     if ($us == 0)
+    //     {
+    //         session()->flash('message','We can\'t find a user with that e-mail address.');
+    //         session()->flash('type','danger');
+    //         return redirect()->back();
+    //     }else{
+    //        $this->userPasswordReset($request->email);
            
-            session()->flash('message','Password Reset Link Send Your E-mail');
-            session()->flash('type','success');
-            return redirect()->back();
-        }
+    //         session()->flash('message','Password Reset Link Send Your E-mail');
+    //         session()->flash('type','success');
+    //         return redirect()->back();
+    //     }
 
-    }
+    // }
 }
