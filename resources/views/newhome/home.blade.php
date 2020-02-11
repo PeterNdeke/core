c6a32955a0e38c174819ca83ad83a28712031ddb
PeterNdeke <ndekepeter2015@gmail.com>
2020-02-11 23:17:19 +0100

    commit

--- resources/views/newhome/home.blade.php
+++ resources/views/newhome/home.blade.php
@@ -818,7 +818,7 @@
                                 <div class="mb-1">
                                     <span class="small text-muted">Returns</span><br>
                                 <span class="h3 font-weight-bold text-primary">{{$item->percent}}%</span>
-                                <small>per {{$item->time}} months</small>
+                                <small>in {{$item->time}} months</small>
                                 <br>
                                  
 
