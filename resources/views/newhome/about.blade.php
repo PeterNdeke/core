0cf1c5662868976ddd41f8fd97c82d41ea8181f9
PeterNdeke <ndekepeter2015@gmail.com>
2020-03-14 18:01:05 +0100

    commit

--- resources/views/newhome/about.blade.php
+++ resources/views/newhome/about.blade.php
@@ -18,6 +18,9 @@
 
     <div class="videos-wrapper video--one section-padding about-us-page">
       <div class="container">
+        <div class="divider divider-simple text-left">
+            <h2 class="m-bottom-20">Company Overview</h2>
+        </div>
           <div class="row align-items-md-center">
               <div class="col-lg-5 col-md-6 margin-sm-60">
                   
@@ -44,58 +47,59 @@
       </div>
   </div><!-- ends: .videos-wrapper -->
 
-  <section class="singl-testimonial shadow"><div class="container-fluid py-0"><div class="row align-items-center gradient gradient-54 color-1"><div class="col-md-4 mx-auto py-4 py-md-0"><p class="font-md bold mt-1 color-1">MISSION</p><hr class="my-4"><p class="lead color-1"> Zircon-Laurel Capital Investment Africa(ZCPI) seeks to create and promote common ground for people to partner with us, rest after investing their capital and make withdrawal at the maturity period. We look for opportunity where we can dig in holes and share within our partners.</p></div><div class="col-12 col-md-6 image-background cover" style="background-image: url(asset/img/about.jpeg);"></div></div></div></section>
-
-  <!-- ./Top-notch appearance - Big screen left -->
-  <section class="section bottom-right bg-1">
-    <div class="container-wide bring-to-front">
-        <div class="row gap-y align-items-center text-center text-lg-left">
-            <div class="col-10 col-lg-5 mx-auto order-md-2">
-                <h2>DEVOTED TO OUR CLIENT</h2>
-                <p class="lead color-2">Got formation Limited which was incorporated under the companies Act of 1990 with registration Number –RC 880753 registered 2010 and began operation fully is a subsidiary of Zircon Laurel Investment Limited. Ever since, we’ve been devoted to investment and partnership from prospect  to client, bringing  goodness into every sector we invest in and inviting millions of people to join us as we move.</p></div>
-            <div class="col-12 col-lg-6 order-md-1"><img src="asset/img/v2.png" alt="" class="img-responsive" style="max-width: 80%;" data-aos="fade-right">
-            </div>
+  <section class="section-padding section-bg">
+    <div class="container">
+        <div class="row align-items-center">
+           
+            <div class="col-lg-5 margin-md-60">
+                <h2>MISSION</h2>
+                <p>Zircon-Laurel Capital Investment Africa(ZCPI) seeks to create and promote common ground for people to partner with us, rest after investing their capital and make withdrawal at the maturity period. We look for opportunity where we can dig in holes and share within our partners.</p>
+               
+            </div><!-- ends: .col-lg-5 -->
+            <div class="col-lg-6 offset-lg-1">
+                <img src="{{asset('asset/img/about.jpeg')}}" alt="" class="rounded">
+                </div><!-- ends: .col-lg-6 -->
+           
         </div>
     </div>
-</section>
+</section><!-- ends: section -->
 
-<section class="singl-testimonial shadow">
-    <div class="container-fluid py-0">
-        <div class="row align-items-center gradient gradient-54 color-1">
-            <div class="col-md-5">
-                <img src="asset/img/earn.jpg" alt="" class="img-responsive" style="max-width: 80%;" data-aos="fade-right">
+<section class="content-block content--block3 section-padding">
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-5 margin-md-60">
+                <img src="{{asset('asset/img/africa.jpg')}}" alt="">
+            </div><!-- ends: .col-lg-5 -->
+            <div class="col-lg-6 offset-lg-1">
+                <h4>DEVOTED TO OUR CLIENT</h4>
+                <p> Got formation Limited which was incorporated under the companies Act of 1990 with registration Number –RC 880753 registered 2010 and began operation fully is a subsidiary of Zircon Laurel Investment Limited. Ever since, we’ve been devoted to investment and partnership from prospect to client, bringing goodness into every sector we invest in and inviting millions of people to join us as we move. </p>
+                
+            </div><!-- ends: .col-lg-6 -->
+            <div class="col-lg-12">
+                <hr class="hr">
             </div>
-            <div class="col-md-6 mx-auto py-4 py-md-0">
-                <h2>EARN AS BUSINESS PARTNERSHIP DURATION ENDS</h2>
-                <hr class="my-4">
-                <div class="bold mt-1 color-1">We give space for client to partner with us within duration of time on our investment platform, each partner determine the sector they invest on base on duration, returns are already calculated for you base on the amount of unit(s) you selected, and you earn when the duration ends.</div>
+            <div class="col-lg-6 margin-md-60">
+                <h4>EARN AS BUSINESS PARTNERSHIP DURATION ENDS</h4>
+                <p> We give space for client to partner with us within duration of time on our investment platform, each partner determine the sector they invest on base on duration, returns are already calculated for you base on the amount of unit(s) you selected, and you earn when the duration ends.</p>
+                
+            </div><!-- ends: .col-lg-6 -->
+            <div class="col-lg-5 offset-lg-1">
+                <img src="{{asset('asset/img/earn.jpg')}}" alt="">
+            </div><!-- ends: .col-lg-5 -->
+            <div class="col-lg-12">
+                <hr class="hr">
+            </div>
+            <div class="col-lg-5 margin-md-60">
+            <img src="{{asset('asset/img/naira.jpg')}}" alt="">
+            </div><!-- ends: .col-lg-5 -->
+            <div class="col-lg-6 offset-lg-1">
+                <h4>PARTNER AND SAVE MORE</h4>
+                <p>We are business mindful organization that creates opportunity for client/customer to partner with us and earn to maximize wealth.</p>
+                
+            </div><!-- ends: .col-lg-6 -->
         </div>
     </div>
-</section>
-
-<section class="section bg-6">
-    <div class="container-wide">
-        <div class="row gap-y align-items-center text-center text-lg-left">
-            <div class="col-10 col-lg-6 mx-auto">
-                <h2>PARTNER AND SAVE MORE</h2>
-                <p class="lead color-2">We are business mindful organization that creates opportunity for client/customer to partner with us and earn to maximize wealth</p></div>
-            <div class="col-12 col-lg-5 pr-0"><img src="asset/img/save.jpg" alt="" class="img-responsive" style="max-width: 60%;" data-aos="fade-left"></div>
-        </div>
-    </div>
-</section>
-
-  <div class="hr-divider"></div>
-
-    {{-- <!--about us page content start-->
-    <section class="section-padding about-us-page">
-        <div class="container">
-            <div class="row">
-                <div class="col-md-12">
-                    {!! $basic->about !!}
-                </div>
-            </div>
-        </div>
-    </section> --}}
+</section><!-- ends:.content-block -->
 
     <script src="{{asset('asset/js/plugins.min.js')}}"></script>
     <script src="{{asset('asset/js/script.min.js')}}"></script>
