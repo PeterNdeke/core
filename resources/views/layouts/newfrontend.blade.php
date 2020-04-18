f3a88ce04cf7e540fc818331b7006e95680cf70d
PeterNdeke <ndekepeter2015@gmail.com>
2020-04-18 15:52:32 +0100

    commit

--- resources/views/layouts/newfrontend.blade.php
+++ resources/views/layouts/newfrontend.blade.php
@@ -50,6 +50,30 @@
 <body  data-spy="scroll">
 <!-- Start Pre-Loader-->
 
+<!-- Load Facebook SDK for JavaScript -->
+<div id="fb-root"></div>
+<script>
+  window.fbAsyncInit = function() {
+    FB.init({
+      xfbml            : true,
+      version          : 'v6.0'
+    });
+  };
+
+  (function(d, s, id) {
+  var js, fjs = d.getElementsByTagName(s)[0];
+  if (d.getElementById(id)) return;
+  js = d.createElement(s); js.id = id;
+  js.src = 'https://connect.facebook.net/en_US/sdk/xfbml.customerchat.js';
+  fjs.parentNode.insertBefore(js, fjs);
+}(document, 'script', 'facebook-jssdk'));</script>
+
+<!-- Your customer chat code -->
+<div class="fb-customerchat"
+  attribution=setup_tool
+  page_id="1988612584780194">
+</div>
+
 <div id="preloader">
     <div data-loader="circle-side"></div>
 
