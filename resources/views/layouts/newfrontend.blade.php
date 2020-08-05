4894dc5f423faea4e3736f6c029e37b4034904f1
PeterNdeke <ndekepeter2015@gmail.com>
2020-08-05 14:13:57 +0100

    commit

--- resources/views/layouts/newfrontend.blade.php
+++ resources/views/layouts/newfrontend.blade.php
@@ -128,10 +128,10 @@
                     <li><a href="{{ url('plans') }}">Investment</a></li>
                 {{-- <li><a class="" href="{{url('properties')}}">Properties</a></li>
                 <li><a class="" href="{{url('sectors')}}">Sectors</a></li> --}}
-                <li><a class="page-scroll" href="#">Sectors<i class="fa fa-angle-down""></i></a>
-                             <ul class="mega-menu mega-menu1 mega-menu2 menu-postion-4" style=" right:220px !important;">
+                <li><a class="page-scroll" href="#">Sectors<i class="fa fa-angle-down"></i></a>
+                             <ul class="mega-menu mega-menu1 mega-menu2 menu-postion-4">
 							
-                               
+                                
                              <li class="mega-list mega-list1"><a class="page-scroll" href="{{url('sector-details/agricultural-sector')}}">Agricultural Sector</a></li>
                              <li class="mega-list mega-list1"><a class="page-scroll" href="{{url('properties')}}">Real Estate Sector</a></li>
                              <li class="mega-list mega-list1"><a class="page-scroll" href="#">Oil and Gas Sector</a></li>
